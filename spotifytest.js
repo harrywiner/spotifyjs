646e7b8d37aac6a3f0bfd6eb96f81d57e3c175b9
Harry Winer <winer.harry@gmail.com>
2020-05-01 00:03:15 +0100

    added multiple file capability

--- spotifytest.js
+++ spotifytest.js
@@ -17,6 +17,9 @@ var con = mysql.createConnection({
 
 con.connect(function(err) {
     if (err) throw err;
+    /*con.query("DROP TABLE plays; CREATE TABLE plays(playID INT AUTO_INCREMENT,trackName varchar (32),artistName varchar (32),endTime varchar (32),msPlayed INT,PRIMARY KEY (playID));", function (err, result, fields) {
+        if (err) throw err;
+    });*/
 });
 
 function addToDatabase(play) {
@@ -35,13 +38,19 @@ function formatString(str) {
    return formatStr;
 }
 
-function main() {
-    var recentPlays = ReadRecents('./json/sample.json');
+function insertRecents(filename) {
+    var recentPlays = ReadRecents(filename);
     for(i = 0; i < recentPlays.length; i++) {
         recentPlays[i].trackName = formatString(recentPlays[i].trackName);
         recentPlays[i].artistName = formatString(recentPlays[i].artistName);
         addToDatabase(recentPlays[i]);
     }
+}
+
+function main() {
+    insertRecents('./json/StreamingHistory0.json');
+    insertRecents('./json/StreamingHistory1.json');
+    insertRecents('./json/StreamingHistory2.json');
 
     //console.log(con.query('select trackName, sum(msPlayed) as timeListened from plays where trackName like "Althea%" group by trackname order by timeListened desc;'))
     con.end();
--- sqltest.js
+++ sqltest.js
@@ -9,10 +9,17 @@ var con = mysql.createConnection({
 
 con.connect(function(err) {
     if (err) throw err;
-        con.query("INSERT INTO people (name, age) VALUES ('Janet', 21)", function (err, result, fields) {
-            if (err) throw err;
-            console.log(result);
-            con.end();
-        });
+    //con.query("INSERT INTO people (name, age) VALUES ('Janet', 21)", function (err, result, fields) {
+
+    var sql = "select * from people where name = ?;";
+    var inserts = ['Harry'];
+
+    sql = con.format(sql, inserts);
+    
+    con.query(sql, function (err, result, fields) {
+        if (err) throw err;
+        console.log(result);
+        con.end();
+    });
 });
 
